af4f8358a5980ff35bb187cbb7552f89f972f280
phcraftboxx <ph@craftboxx.de>
2022-02-10 09:40:22 +0100

    Forms - Form Request Classes

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePost;
 use App\Models\BlogPost;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -38,18 +39,14 @@ class PostsController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store(StorePost $request)
     {
 
-        $request->validate([
-            'title'   => 'required|min:5|max:100',
-            'content' => 'required|min:5'
-        ]);
+        $validated = $request->validated();
 
         $post          = new BlogPost();
-        $post->title   = $request->input('title');
-
-        $post->content = $request->input('content');
+        $post->title   = $validated['title'];
+        $post->content = $validated['content'];
         $post->save();
 
         return redirect()->route('posts.show', ['post' => $post->id]);
