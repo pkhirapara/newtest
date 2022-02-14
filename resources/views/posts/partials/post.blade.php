a9f489b0c9ff9a1c50be03ff0fa265046b8e1bd3
phcraftboxx <ph@craftboxx.de>
2022-02-14 09:46:06 +0100

    Styling Post List Page

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -8,13 +8,14 @@
     </div>
 @endif--}}
 
-<div>{{ $key }}. {{ $post->title }}</div>
+<h3><a href="{{ route('posts.show', ['post' => $post->id]) }}">{{ $post->title }}</a></h3>
 
-<div>
-    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+<div class="mb-3">
+    <a href="{{ route('posts.edit', ['post' => $post->id]) }}" class="btn btn-primary">Edit</a>
+    <form class="d-inline" action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
         @csrf
         @method('DELETE')
 
-        <input type="submit" value="Delete!">
+        <input class="btn btn-primary" type="submit" value="Delete!">
     </form>
 </div>
