07fc304a50f05913b0b52543d790066a2ee99a59
phcraftboxx <ph@craftboxx.de>
2022-02-16 10:30:03 +0100

    Testing for failure: test_store_fail

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -50,4 +50,22 @@ class PostTest extends TestCase
 
         $this->assertEquals(session('status'), 'The blog post was created!');
     }
+
+    public function test_store_fail()
+    {
+        $params = [
+            'title' => 'd',
+            'content' => 'e'
+        ];
+
+        $this->post('/posts', $params)
+            ->assertStatus(302)
+            ->assertSessionHas('errors');
+
+        $messages = session('errors')->getMessages();
+
+        $this->assertEquals($messages['title'][0], 'The title must be at least 5 characters.');
+        $this->assertEquals($messages['content'][0], 'The content must be at least 5 characters.');
+
+    }
 }
